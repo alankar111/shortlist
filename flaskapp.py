20abee47d87d869b50ca93693aa4c52ab26072f3
alankar <alankar111@gmail.com>
2016-07-31 12:47:15 +0530

    Adding basic jinja rules

--- flaskapp.py
+++ flaskapp.py
@@ -10,7 +10,14 @@ def shutdown_session(exception=None):
     db_session.remove()
 
 @app.route('/')
-def hello_world():
+def home():
 	return render_template('index.html')
-    # return render_template('question.html')
     # return "Hello World!"
+
+@app.route('/<id>')
+def next_question(id):
+	prev_answers = request.form['prev_answers']
+	return render_template('index.html', question=question, prev_answers=prev_answers)
+
+def should_ask_question(id, prev_answers):
+	return True
